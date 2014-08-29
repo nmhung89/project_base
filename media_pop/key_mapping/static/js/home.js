07f3a93b4793b3efe4b35e827f43debd9382efb4
minhtra-lunex <minhtra@lunextelecom.com>
2014-08-29 15:31:40 +0700

    update

--- media_pop/key_mapping/static/js/home.js
+++ media_pop/key_mapping/static/js/home.js
@@ -1,11 +1,50 @@
-$(document).on("keypress", function(e) { 
-	if ( e.ctrlKey ) {
-		var prefix = 'CTRL';
+$(document).keydown(function(e) {
+	var prefix = '';
+	var code = '';
+	if (e.ctrlKey) {
+		prefix = "CTRL";
+		e.preventDefault();
 	}
-	if ( e.altKey ) {
-		var prefix = 'ALT';
+	else if (e.altKey) {
+		prefix = "ALT";
+		e.preventDefault();
 	}
-}
-$('#id_enter_new_definition').click(function() {
+	else 
+		return;
 	
+	if (e.keyCode < 65 || e.keyCode > 122)
+		return;
+	if (e.keyCode >= 97)
+		code = e.keyCode - 32;
+	else
+		code = e.keyCode;
+	var key = prefix + "_" + code;
+	var message = key_mapping[key];
+	if (message == undefined)
+		message = 'This combination was not defined!';
+	alert(message);
+});
+
+$('#id_enter_new_definition').click(function() {
+	var special_key = $('#id_special_key').val();
+	var key = $('#id_key').val();
+	var key_text = $('#id_key option:selected').text();
+	var message = $('#id_message').val();
+	$.ajax({
+	  type: "POST",
+	  url: "/json/input-definition/",
+	  data: { 
+		  		special_key: special_key, 
+		  		key: key,
+		  		message: message
+		  	}
+	})
+	  .done(function(data) {
+		  if (data.Code == 1) {
+			  $('#id_body_table').append('<tr><td>' + special_key + '</td><td>' + key_text + '</td><td>' + message + '</td></tr>');
+		  }
+		  else {
+			  alert(data.Message);
+		  }
+	  });
 });
