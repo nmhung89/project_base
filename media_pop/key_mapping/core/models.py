11cd81cae26ce7d1ee5bbbc87e36d0f3d7858a97
nmhung89 <nmhung89@gmail.com>
2014-09-01 01:18:35 +0700

    update v2

--- media_pop/key_mapping/core/models.py
+++ media_pop/key_mapping/core/models.py
@@ -3,6 +3,9 @@ from django.db import models
 SPECIAL_KEYS = [('CTRL', 'CTRL'), ('ALT', 'ALT')]
 
 class KeyMapping(models.Model):
+    class Meta:
+        unique_together = ('SpecialKey', 'Key')
+        
     SpecialKey = models.CharField(max_length=5, choices=SPECIAL_KEYS)
     Key = models.IntegerField()
     Message = models.CharField(max_length=100)
--- media_pop/key_mapping/urls.py
+++ media_pop/key_mapping/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -13,4 +15,4 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
